c0f7e1a8fa4c9461fa4a4174ff797ddde1579e86
Shaig <shaiqnizamzade@gmail.com>
2025-05-09 20:01:40 +0400

    express node.js

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-var http = require("http"); // node modules
-var fs = require("fs");
-var server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    fs.readFile("index.html", (err, html) => {
-      res.write(html);
-      res.end();
-    });
-  } else if (req.url == "/urunler") {
-    fs.readFile("urunler.html", (err, html) => {
-      res.write(html);
-      res.end();
-    });
-  } else {
-    fs.readFile("404.html", (err, html) => {
-      res.write(html);
-      res.end();
-    });
-  }
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
+const data = [
+  {
+    id: 1,
+    name: "iphone 14",
+    price: 3000,
+    isActive: true,
+    image: "img1.jpg",
+  },
+  {
+    id: 2,
+    name: "samsung s20",
+    price: 2000,
+    isActive: false,
+    image: "img1.jpg",
+  },
+  {
+    id: 3,
+    name: "xiamo 11",
+    price: 1000,
+    isActive: true,
+    image: "img1.jpg",
+  },
+];
+// routes
+app.use("/products/:id", function (req, res) {
+  const urun = data.find((u) => u.id == req.params.id);
+  res.render("product-details", urun);
 });
-
-server.listen(3000, () => {
+app.use("/products", function (req, res) {
+  res.render("products", { liste: data });
+});
+app.use("/", function (req, res) {
+  res.render("index");
+});
+app.listen(3000, () => {
   console.log("node.js server at port 3000");
 });
